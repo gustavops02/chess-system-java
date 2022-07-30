a7ea88701fe552a9738ef33a177bd83012fa8c02
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-07-30 15:41:13 -0300

    Little improvement on terminal

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import chess.ChessMatch;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
+	}
+
+}
