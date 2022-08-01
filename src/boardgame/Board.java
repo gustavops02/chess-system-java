8a5b993cd681baca7c8ca8b30f40ee73f64e11bd
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-08-01 14:26:27 -0300

    Implementing possible moves of Rook.

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -37,7 +37,7 @@ public class Board {
 	}
 	
 	public void placePiece(Piece piece, Position position) {
-		if(ThereIsAPiece(position)) {
+		if(thereIsAPiece(position)) {
 			throw new BoardException("There is already a piece on position " + position);
 		}
 		pieces[position.getRow()][position.getColumn()] = piece;
@@ -67,7 +67,7 @@ public class Board {
 		return positionExists(position.getRow(), position.getColumn());
 	}
 	
-	public boolean ThereIsAPiece(Position position) {
+	public boolean thereIsAPiece(Position position) {
 		if(!positionExists(position) ) {
 			throw new BoardException("Position not on the board.");
 		}
