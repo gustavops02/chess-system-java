bd65b73613c94f279a3c8586a396d7fc9cbc8cb7
Gustavo Paulo <gustavo.psilvaa02@gmail.com>
2022-08-07 16:23:09 -0300

    Queen

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -11,6 +11,7 @@ import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Knight;
 import chess.pieces.Pawn;
+import chess.pieces.Queen;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -209,6 +210,7 @@ public class ChessMatch {
 	        placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
 	        placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
+	        placeNewPiece('d', 1, new Queen(board, Color.WHITE));
 	        placeNewPiece('c', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('d', 2, new Pawn(board, Color.WHITE));
 	        placeNewPiece('e', 2, new Pawn(board, Color.WHITE));
@@ -226,6 +228,7 @@ public class ChessMatch {
 	        placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 	        placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
 	        placeNewPiece('c', 7, new Pawn(board, Color.BLACK));
+	        placeNewPiece('d', 8, new Queen(board, Color.BLACK));
 	        placeNewPiece('d', 7, new Pawn(board, Color.BLACK));
 	        placeNewPiece('e', 7, new Pawn(board, Color.BLACK));
 			placeNewPiece('g', 8, new Knight(board, Color.BLACK));
